544e4890f49fb4c4cbe8ae8cd74bb451d769e3b6
LHFahim <fahimlutful@gmail.com>
2023-09-27 12:49:01 +0600

    added styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Input from "./ui/Input";
+import Row from "./ui/Row";
+
+const StyledApp = styled.main`
+  /* background-color: orangered; */
+  padding: 20px;
+`;
+
 function App() {
   return (
     <>
-      <div>hi</div>
+      <GlobalStyles />
+      <StyledApp>
+        <Row>
+          <Row type="horizontal">
+            <Heading type="h1">The Magic Mountain Cottage</Heading>
+
+            <div>
+              <Heading type="h2">Check in and out</Heading>
+              <Button onClick={() => alert(`Check in!`)}>Check In</Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert(`Check out!`)}
+              >
+                Check Out
+              </Button>
+            </div>
+          </Row>
+
+          <Row>
+            <Heading as="h3">Form</Heading>
+            <form action="">
+              <Input type="number" placeholder="Number of guests"></Input>
+              <Input type="number" placeholder="Number of guests"></Input>
+            </form>
+          </Row>
+        </Row>
+      </StyledApp>
     </>
   );
 }
