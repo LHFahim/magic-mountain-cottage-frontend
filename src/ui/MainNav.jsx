93e6d2604401914fc5932fdd65c793d5e21e2f9a
LHFahim <fahimlutful@gmail.com>
2023-09-27 14:02:33 +0600

    added header, sidebar

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const NavList = styled.ul`
@@ -6,7 +8,7 @@ const NavList = styled.ul`
   gap: 0.8rem;
 `;
 
-const Link = styled.a`
+const StyledNavLink = styled(NavLink)`
   &:link,
   &:visited {
     display: flex;
@@ -44,3 +46,20 @@ const Link = styled.a`
     color: var(--color-brand-600);
   }
 `;
+
+const MainNav = () => {
+  return (
+    <nav>
+      <NavList>
+        <li>
+          <StyledNavLink to="/dashboard">Home</StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to="/bookings">Booking</StyledNavLink>
+        </li>
+      </NavList>
+    </nav>
+  );
+};
+
+export default MainNav;
